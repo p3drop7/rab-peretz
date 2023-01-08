7fb4fb372cbac0b3eccf94dee42c354e55bf9918
Pedro Palencia <pedropalencia7@gmail.com>
2023-01-08 00:23:44 +0000

    book comments added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// NAV BAR
 const navbar = document.querySelector('nav');
 const navbarLink = document.querySelectorAll('.nav-link-selector')
 const navExpandButton = document.querySelector('#nav-display-button')
@@ -37,3 +38,49 @@ navLinks.forEach(item => {
         navbar.classList.remove('nav-visible')
     })
 });
+
+// BOOK COMMENTS (Jquery)
+
+//Book comment 1
+$('#book-pic-1').click(()=> $('#book-comment-1').fadeIn('slow'));
+$('#book-comment-1').click(()=> $('#book-comment-1').fadeOut('slow'));
+
+//Book comment 2
+$('#book-pic-2').click(()=> $('#book-comment-2').fadeIn('slow'));
+$('#book-comment-2').click(()=> $('#book-comment-2').fadeOut('slow'));
+
+//Book comment 3
+$('#book-pic-3').click(()=> $('#book-comment-3').fadeIn('slow'));
+$('#book-comment-3').click(()=> $('#book-comment-3').fadeOut('slow'));
+
+// BOOK COMMENTS IN MOBILE DESIGN (Jquery)
+
+//Book comment 1
+$('#carrousel-book-1').click(()=> {
+    $('#book-comment-1--mobile').fadeIn('slow')
+    $('#carrousel-book-1').fadeOut('slow')
+});
+$('#book-comment-1--mobile').click(()=> {
+    $('#book-comment-1--mobile').fadeOut('slow')
+    $('#carrousel-book-1').fadeIn('slow')
+})
+
+//Book comment 2
+$('#carrousel-book-2').click(()=> {
+    $('#book-comment-2--mobile').fadeIn('slow')
+    $('#carrousel-book-2').fadeOut('slow')
+});
+$('#book-comment-2--mobile').click(()=> {
+    $('#book-comment-2--mobile').fadeOut('slow')
+    $('#carrousel-book-2').fadeIn('slow')
+})
+
+//Book comment 3
+$('#carrousel-book-3').click(()=> {
+    $('#book-comment-3--mobile').fadeIn('slow')
+    $('#carrousel-book-3').fadeOut('slow')
+});
+$('#book-comment-3--mobile').click(()=> {
+    $('#book-comment-3--mobile').fadeOut('slow')
+    $('#carrousel-book-3').fadeIn('slow')
+})
